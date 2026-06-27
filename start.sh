4e8a33cb114ef21485f4336d1ee588f4c37ff265
ABDULLAH <166320271+mdabdullahsk@users.noreply.github.com>
2026-06-27 17:44:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# DBUS সার্ভিস চালু করা
 service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
+
+# PulseAudio সিস্টেম মোডে চালু করা
+pulseaudio --start --system --disallow-exit --disable-shm --realtime=no
+
+# XRDP সার্ভিস চালু করা
 service xrdp start
 
+# X11 permissions ঠিক করা
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
+# কনটেইনার সচল রাখা
 tail -f /var/log/xrdp-sesman.log
